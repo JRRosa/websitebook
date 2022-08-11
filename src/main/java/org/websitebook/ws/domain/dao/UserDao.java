3272934974321732aac1664852b3bed9d0737c60
JRRosa <jorgereyesrosa@gmail.com>
2022-08-11 13:40:01 -0400

    dev-t12: fit add exception

--- src/main/java/org/websitebook/ws/domain/dao/UserDao.java
+++ src/main/java/org/websitebook/ws/domain/dao/UserDao.java
@@ -3,10 +3,11 @@ package org.websitebook.ws.domain.dao;
 import java.util.List;
 
 import org.websitebook.ws.domain.dao.entities.User;
+import org.websitebook.ws.domain.dao.exceptions.DBException;
 
 public interface UserDao extends Dao<User, Long> {
 	
-	List<User> findAll();
-	User findById(Long id);
+	List<User> findAll() throws DBException;
+	User findById(Long id) throws DBException;
 	
 }
